4105cf5d61a89562972419ca9a5f9d74cb3be8be
“jinyounzz” <realyoung0705@icloud.com>
2026-07-11 01:04:26 +0900

    ok

---
+++ ㅇㅇㅇ.py
@@ -0,0 +1,15 @@
+def vaild(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    for i in range(9):
+        if board[col][i] == num:
+            return False
+   start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i]{start_col + j} == num:
+                return False
+    return True
+
+def solve(board):
